77f492a2be82dbed1aa8bacc2086ef2bd576a64a
ChaitanyaS <chaitanya.s@pei.group>
2025-03-01 13:35:34 +0530

    Updated Intro.

--- components/intro.tsx
+++ components/intro.tsx
@@ -30,9 +30,10 @@ export default function Intro() {
               duration: 0.2,
             }}
           >
+            {/* Change Image*/}
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              alt="Chaitanya portrait"
               width="192"
               height="192"
               quality="95"
@@ -62,11 +63,12 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Chaitanya.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">5 years</span> of experience. I am a tech
+        enthusiast with a love for building{" "}
+        <span className="italic">scalable applications</span>. My expertise is
+        in <span className="underline">Node and Java</span>.
       </motion.h1>
 
       <motion.div
